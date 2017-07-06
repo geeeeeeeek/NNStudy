9c5f4960a8e7c5baa53dcc512543b685ef438704
xiao qingsong <xiaoqingsong@xiaodeMacBook-Pro.local>
2017-07-06 23:48:04 +0800

    Added test04.py

--- test03.py
+++ test03.py
@@ -1,9 +1,12 @@
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf 
 
-w1 = tf.Variable(tf.random_normal([2,2], stddev=1, seed=1))
-w2 = tf.Variable(tf.random_normal([3,2], stddev=1, seed=1))
+w1 = tf.Variable(tf.random_normal([2,3], stddev=1, seed=1))
+w2 = tf.Variable(tf.random_normal([3,1], stddev=1, seed=1))
 
-x = tf.constant([0.7,0.9])
+x = tf.constant([[0.7,0.9],[1.0,3.0]])
 
 
 a = tf.matmul(x,w1)
