4c9e706c174ec36ebe6679d00c9d1c98aa262b6b
xiao qingsong <xiaoqingsong@xiaodeMacBook-Pro.local>
2017-07-06 20:47:24 +0800

    first commit

---
+++ test01.py
@@ -0,0 +1,10 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+# print(sess.run(hello))
+
+a = tf.constant(10)
+b = tf.constant(32)
+print(sess.run(a+b))
---
+++ test03.py
@@ -0,0 +1,22 @@
+import tensorflow as tf 
+
+w1 = tf.Variable(tf.random_normal([2,3], stddev=1, seed=1))
+w2 = tf.Variable(tf.random_normal([3,2], stddev=1, seed=1))
+
+x = tf.constant([0.7,0.9])
+
+print 'hhh'
+
+a = tf.matmul(x,w1)
+y = tf.matmul(a,w2)
+
+print 'kkk'
+
+sess = tf.Session()
+
+sess.run(w1.initializer)
+sess.run(w2.initializer)
+
+print(sess.run(y))
+
+sess.close()
