9b22c8a4ad699e483dd2e6a3b98a71e2815c412b
xiao qingsong <xiaoqingsong@xiaodeMacBook-Pro.local>
2017-07-28 00:02:41 +0800

    added some code

--- test13.py
+++ test13.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
-import numpy as np
 import matplotlib.pyplot as plt
 
-image_raw_data = tf.gfile.FastGFile("images/", "r",).read()
+image_raw_data = tf.gfile.FastGFile("images/mock.png", "r",).read()
 
 with tf.Session() as sess:
-    img_data = tf.image.decode_jpeg(image_raw_data)
+
+    # 解码图像
+    img_data = tf.image.decode_png(image_raw_data)
 
     print img_data.eval()
+
+    # 展示图像
+    plt.imshow(img_data.eval())
+    plt.show()
+
+    # 转化为实数类型
+    img_data = tf.image.convert_image_dtype(img_data, dtype=tf.uint16)
+
+    # jpeg编码
+    encoded_image = tf.image.encode_png(img_data)
